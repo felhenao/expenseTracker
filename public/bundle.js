3e3d58516fa79e3ecf0b192056186568df5d5796
Felipe <felhenao@gmail.com>
2021-02-24 19:27:52 -0500

    Added boilerplate

---
+++ public/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/app.js":
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: Plugin/Preset files are not allowed to export objects, only functions. In /Users/doneright/Desktop/prog/projects/React/boilerplate expense-tracker  copy/node_modules/babel-preset-react/lib/index.js\n    at createDescriptor (/Users/doneright/Desktop/prog/projects/React/boilerplate expense-tracker  copy/node_modules/@babel/core/lib/config/config-descriptors.js:203:11)\n    at createDescriptor.next (<anonymous>)\n    at step (/Users/doneright/Desktop/prog/projects/React/boilerplate expense-tracker  copy/node_modules/gensync/index.js:261:32)\n    at evaluateAsync (/Users/doneright/Desktop/prog/projects/React/boilerplate expense-tracker  copy/node_modules/gensync/index.js:291:5)\n    at /Users/doneright/Desktop/prog/projects/React/boilerplate expense-tracker  copy/node_modules/gensync/index.js:44:11\n    at Array.forEach (<anonymous>)\n    at Function.async (/Users/doneright/Desktop/prog/projects/React/boilerplate expense-tracker  copy/node_modules/gensync/index.js:43:15)\n    at Function.all (/Users/doneright/Desktop/prog/projects/React/boilerplate expense-tracker  copy/node_modules/gensync/index.js:216:13)\n    at Generator.next (<anonymous>)\n    at createDescriptors (/Users/doneright/Desktop/prog/projects/React/boilerplate expense-tracker  copy/node_modules/@babel/core/lib/config/config-descriptors.js:134:49)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/app.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }, {
+      test: /\.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
+};

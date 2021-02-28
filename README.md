90be05b955ce9ebb2d6ccf4995e87563ad453834
Felipe <felhenao@gmail.com>
2021-02-28 15:22:40 -0500

    Added Redux actions, reducers, selectors and store

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # expenseTracker
+
+## How to run
+- While in root directory => yarn install
+
+Start dev server:
+- yarn run start
+
+
+
+
--- src/app.js
+++ src/app.js
@@ -3,7 +3,22 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'water bill' }));
+store.dispatch(addExpense({ description: 'gas bill' }));
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const visibleExpenes = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenes);
+
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
 

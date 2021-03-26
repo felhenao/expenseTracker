fe5964ec6ab446f674f45e99aa9a75f4f3c090d0
Felipe <felhenao@gmail.com>
2021-03-26 18:43:04 -0400

    Added logout functionality

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -7,7 +7,6 @@ export const startLogin = () => {
       .then((result) => {
           /** @type {firebase.auth.OAuthCredential} */
           const credential = result.credential;
-
           // This gives you a Google Access Token. You can use it to access the Google API.
           const token = credential.accessToken;
           // The signed-in user info.
@@ -22,6 +21,17 @@ export const startLogin = () => {
           // The firebase.auth.AuthCredential type that was used.
           const credential = error.credential;
           // ...
+      });
+    };
+  };
+
+  export const startLogout = () => {
+    return () => {
+      return firebase.auth().signOut().then(() => {
+        console.log('You are logged out')
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
       });
     };
   };

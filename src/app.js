96a076c507e9e477368c99caf5d8e0c4cbdde2d6
Felipe <felhenao@gmail.com>
2021-04-07 18:00:46 -0400

    Refactored app.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
+import { startSetExpenses } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import configureStore from './store/configureStore';
+import { firebase } from './firebase/firebase';
+import { login, logout } from './actions/auth';
 
 const store = configureStore();
-
 const jsx = (
     <Provider store={store}>
-        <AppRouter />
+      <AppRouter />
     </Provider>
-);
+  );
 
-ReactDOM.render(<p>loading...</p>, document.getElementById('root'));
-ReactDOM.render(jsx, document.getElementById('root'));
+let hasRendered = false;
+const renderApp = () => {
+    if (!hasRendered) {
+        ReactDOM.render(jsx, document.getElementById('root'));
+        hasRendered = true;
+    }
+};
 
+ReactDOM.render(<p>Loading...</p>, document.getElementById('root'));
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        store.dispatch(login(user.uid));
+        store.dispatch(startSetExpenses()).then(() => {
+        renderApp();
+        history.push('/dashboard');
+        if (history.location.pathname === '/') {
+        }
+        }); 
+    } else {
+        store.dispatch(logout());
+        renderApp();
+        history.push('/');
+    }
+});
